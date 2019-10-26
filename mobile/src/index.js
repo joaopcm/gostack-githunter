9fbe11ea5a99503e47f5f4d01f291248bf97749f
Joao Melo <jopcmelo@gmail.com>
2019-10-26 17:29:32 -0300

    chore: Add Reactotron

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { SafeAreaView, ScrollView, Text, StatusBar } from 'react-native';
 
+import './config/ReactotronConfig';
+
 export default function App() {
   return (
     <>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
         <ScrollView contentInsetAdjustmentBehavior="automatic" />
-        <Text>Hello world!</Text>
+        <Text>Hello world!!</Text>
       </SafeAreaView>
     </>
   );
